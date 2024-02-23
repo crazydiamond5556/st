2d08fb583e497590be68057798631aade6733b03
crazydiamond5556 <158544606+crazydiamond5556@users.noreply.github.com>
2024-02-22 20:48:44 -0500

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Inconsolata:pixelsize=19:antialias=true:autohint=true";
+static char *font = "Inconsolata:pixelsize=20:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -105,9 +105,9 @@ static const char *colorname[] = {
 	"#ebdbb2",
 	[255] = 0,
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#282828",   /* 256 -> bg */
-	"#ebdbb2",   /* 257 -> fg */
-	"#add8e6", /* 258 -> cursor */
+	"#121314",   /* 256 -> bg */
+	"#bbbbbb",   /* 257 -> fg */
+	"#eeeeee", /* 258 -> cursor */
 };
 
 
